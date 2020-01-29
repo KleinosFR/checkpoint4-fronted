7f9a91da1b382edbc4f422beac9ca2ed775f8595
KleinosFR <kleinos@hotmail.com>
2020-01-29 18:20:05 +0100

    created form for pic upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ImageUpload from "./Components/ImageUpload";
 
 function App() {
-    return <div className="App"> Toto</div>;
+    return (
+        <div className="App">
+            {" "}
+            <ImageUpload />{" "}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/ImageUpload.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Form, Input, Button, Media } from "reactstrap";
+import axios from "axios";
+
+const apiUrl = "http://localhost:8000";
+
+function ImageUpload() {
+    const [image, setImage] = useState(null);
+    const [previewImage, setPreviewImage] = useState(null);
+
+    useEffect(() => {
+        if (image) {
+            let reader = new FileReader();
+            reader.onloadend = () => {
+                setPreviewImage(reader.result);
+            };
+            reader.readAsDataURL(image);
+        }
+    }, [image]);
+
+    const handleChange = e => {
+        setImage(e.target.files[0]);
+    };
+
+    const resetImage = () => {
+        setPreviewImage(null);
+        setImage(null);
+    };
+
+    const handleUpload = () => {
+        const formData = new FormData();
+        formData.append("image", image);
+
+        axios
+            .post(`${apiUrl}/mediaobject`, formData)
+            .then(res => console.log(res));
+    };
+
+    return (
+        <Form>
+            <Media left>
+                <Media
+                    object
+                    src={previewImage}
+                    alt="Generic placeholder image"
+                />
+            </Media>
+            <Input type="file" onChange={e => handleChange(e)} />
+            <Button onClick={() => handleUpload()}>Envoyer</Button>
+            <Button onClick={() => resetImage()}>Effacer l'image</Button>
+        </Form>
+    );
+}
+
+export default ImageUpload;
